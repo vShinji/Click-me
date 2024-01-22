588f95c6f2c196daf8b758f87913b4e9e493c45c
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-22 17:37:35 +0900

    Toothless Dancing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const button = document.querySelector(".btn");
 const audio = document.getElementById("my-audio");
+const toothless = document.getElementById("toothless");
+
 
 function play() {
   audio.play();
@@ -8,4 +10,6 @@ function play() {
 button.addEventListener("click", function(e) {
   e.preventDefault();
   e.target.classList.add("d-none");
+  toothless.classList.remove("d-none");
+  document.title = "Toothless Dance!";
 });
