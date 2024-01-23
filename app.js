61bcb69c908318e20900f3da250185b3cc6be3db
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-23 18:45:44 +0900

    Music visualizer working

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ button.addEventListener("click", function(e) {
   analyser = audioContext.createAnalyser();
   audioSource.connect(analyser);
   analyser.connect(audioContext.destination);
-  analyser.fftSize = 518;
+  analyser.fftSize = 2048;
 
   const bufferLength = analyser.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
@@ -52,7 +52,7 @@ function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
     barHeight = dataArray[i] * 1.5;
     ctx.save();
     ctx.translate(canvas.width/2, canvas.height/2);
-    ctx.rotate(i * Math.PI *10 / bufferLength);
+    ctx.rotate(i * Math.PI * 10 / bufferLength);
     const hue = i * 0.3;
     ctx.fillStyle = "hsl(" + hue + ",100%," + barHeight/3 + "%)";
     ctx.fillRect(0, 0, barWidth, barHeight);
