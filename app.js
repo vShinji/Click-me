8b9a2424e33f35f835f7d0f9b924f60a0efa2c3e
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-23 17:27:35 +0900

    Started canvas

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const button = document.querySelector(".btn");
 const audio = document.getElementById("my-audio");
 const toothless = document.querySelector(".toothless");
+const canvas = document.querySelector(".canvas1");
+const ctx = canvas.getContext("2d");
 
+let audioSource;
+let analyser;
 
-function play() {
-  audio.play();
-  audio.addEventListener("playing", function() {
-    console.log("audio playing");
-  });
-  audio.addEventListener("ended", function() {
-    console.log("audio ended");
-  });
-};
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 
 button.addEventListener("click", function(e) {
   e.preventDefault();
@@ -21,4 +19,37 @@ button.addEventListener("click", function(e) {
   setTimeout(() => {
     document.body.classList.add("background-active");
   }, 16500);
+
+  const audioCtx = new AudioContext();
+  const audio1 = document.querySelector(".audio1");
+  audio1.src = "";
+
+  audio.play();
+  audioSource = audioContext.createMediaElementsSource(audio1);
+  analyser = audioContext.createAnalyser();
+  audioSource.connect(analyser);
+  analyser.connect(audioContext.destination);
+  analyser.fftSize = 64;
+
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  const barWidth = canvas.width/bufferLength;
+  let barHeight;
+  let x;
+
+  canvas.classList.remove("d-none");
+
+  function animate() {
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+      ctx.fillStyle = "white";
+      ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+      x += barWidth;
+    }
+    requestAnimationFrame(animate);
+  }
+  animate();
 });
